73fa76852c455dffc9cfc3a38bfae53d1ae345f9
Artyom Trityak <art.trityak@gmail.com>
2016-03-23 22:48:07 -0700

    ESLint rules

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,14 +1,13 @@
-'use strict';
-
+import App from './components/app';
+import AppHomeRoute from './routes/app-home-route';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Relay from 'react-relay';
 
-export default class extends Relay.Route {
-  static queries = {
-    viewer: () => Relay.QL`
-      query {
-        viewer
-      }
-    `,
-  };
-  static routeName = 'AppHomeRoute';
-}
+ReactDOM.render(
+  <Relay.RootContainer
+    Component={App}
+    route={new AppHomeRoute()}
+  />,
+  document.getElementById('root')
+);
